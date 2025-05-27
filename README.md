832718efeec7372ddb81528d1b858dcba8303b31
Max Shapira <max.shapira@icloud.com>
2025-05-27 17:37:04 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# LMNTiX Landing Lage
+
+HTML landing page.
+
+## Getting started
+
+* First, ensure that node.js & pnpm are both installed.
+* Run `pnpm install` to install all of the dependencies into your project.
+* When `pnpm` has finished with the install, run `pnpm run build` to recompile the `style.css` file in the root directory.
+
+You're ready to go! The most useful task for rapid development is `pnpm run dev`, which rebuild the CSS every time you make a change in the HML or JS files.
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+	content: ["./**/*.html", "./js/**/*.js"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			colors: {
+				"lmntix-primary": "#1A3A5E",
+				"lmntix-accent-teal": "#00A19D",
+				"lmntix-accent-coral": "#FF6B35",
+				"lmntix-accent-coral-muted": "#E05A2D",
+				"lmntix-helper": "#44546A",
+				// "lmntix-light-bg": "#F7FCFC",
+				"lmntix-white": "#FFFFFF",
+			},
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				"inter-tight": ["Inter Tight", "sans-serif"],
+			},
+			fontSize: {
+				xs: ["0.75rem", { lineHeight: "1.5" }],
+				sm: ["0.875rem", { lineHeight: "1.5715" }],
+				base: ["1rem", { lineHeight: "1.5", letterSpacing: "-0.017em" }],
+				lg: ["1.125rem", { lineHeight: "1.5", letterSpacing: "-0.017em" }],
+				xl: ["1.25rem", { lineHeight: "1.5", letterSpacing: "-0.017em" }],
+				"2xl": ["1.5rem", { lineHeight: "1.415", letterSpacing: "-0.017em" }],
+				"3xl": [
+					"1.875rem",
+					{ lineHeight: "1.3333", letterSpacing: "-0.017em" },
+				],
+				"4xl": ["2.25rem", { lineHeight: "1.2777", letterSpacing: "-0.017em" }],
+				"5xl": ["3rem", { lineHeight: "1", letterSpacing: "-0.017em" }],
+				"6xl": ["4rem", { lineHeight: "1", letterSpacing: "-0.017em" }],
+				"7xl": ["4.5rem", { lineHeight: "1", letterSpacing: "-0.017em" }],
+			},
+			keyframes: {
+				swing: {
+					"0%, 100%": { transform: "translateY(0px)" },
+					"50%": { transform: "translateY(-100px)" },
+				},
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/forms")],
+};
